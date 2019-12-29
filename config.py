4401c0508bbf3570ed2f8c86e3c06fa8bd6d5856
raffe <raffe7@gmail.com>
2019-12-29 20:24:25 +0800

    Signed-off-by: raffe <raffe7@gmail.com>

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Wgwl$5000@192.168.1.10:3306/test?charset=utf8'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+DEBUG = True
+SECRET_KEY = os.urandom(24)
